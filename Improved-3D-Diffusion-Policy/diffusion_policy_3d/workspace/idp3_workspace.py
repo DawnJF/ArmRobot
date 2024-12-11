387e14aa650238d0b74b213139738d3ad0d90d32
Jianfei Ma <majianfei1001@gmail.com>
2024-12-11 11:26:47 +0800

    just format code

--- Improved-3D-Diffusion-Policy/diffusion_policy_3d/workspace/idp3_workspace.py
+++ Improved-3D-Diffusion-Policy/diffusion_policy_3d/workspace/idp3_workspace.py
@@ -22,7 +22,9 @@ from termcolor import cprint
 import shutil
 import time
 from diffusion_policy_3d.workspace.base_workspace import BaseWorkspace
-from diffusion_policy_3d.policy.diffusion_pointcloud_policy import DiffusionPointcloudPolicy
+from diffusion_policy_3d.policy.diffusion_pointcloud_policy import (
+    DiffusionPointcloudPolicy,
+)
 from diffusion_policy_3d.common.checkpoint_util import TopKCheckpointManager
 from diffusion_policy_3d.common.json_logger import JsonLogger
 from diffusion_policy_3d.common.pytorch_util import dict_apply, optimizer_to
@@ -31,13 +33,13 @@ from diffusion_policy_3d.model.common.lr_scheduler import get_scheduler
 
 OmegaConf.register_new_resolver("eval", eval, replace=True)
 
+
 class iDP3Workspace(BaseWorkspace):
-    include_keys = ['global_step', 'epoch']
+    include_keys = ["global_step", "epoch"]
 
     def __init__(self, cfg: OmegaConf, output_dir=None):
         super().__init__(cfg, output_dir=output_dir)
-        
-        
+
         # set seed
         seed = cfg.training.seed
         torch.manual_seed(seed)
@@ -51,13 +53,13 @@ class iDP3Workspace(BaseWorkspace):
         if cfg.training.use_ema:
             try:
                 self.ema_model = copy.deepcopy(self.model)
-            except: # minkowski engine could not be copied. recreate it
+            except:  # minkowski engine could not be copied. recreate it
                 self.ema_model = hydra.utils.instantiate(cfg.policy)
-
 
         # configure training state
         self.optimizer = hydra.utils.instantiate(
-            cfg.optimizer, params=self.model.parameters())
+            cfg.optimizer, params=self.model.parameters()
+        )
 
         # configure training state
         self.global_step = 0
@@ -65,7 +67,7 @@ class iDP3Workspace(BaseWorkspace):
 
     def run(self):
         cfg = copy.deepcopy(self.cfg)
-        
+
         if cfg.training.debug:
             cfg.training.num_epochs = 40
             cfg.training.max_train_steps = 10
@@ -81,8 +83,7 @@ class iDP3Workspace(BaseWorkspace):
             RUN_ROLLOUT = True
             RUN_CKPT = True
             verbose = False
-        
-        
+
         # resume training
         if cfg.training.resume:
             lastest_ckpt_path = self.get_checkpoint_path()
@@ -108,22 +109,17 @@ class iDP3Workspace(BaseWorkspace):
             cfg.training.lr_scheduler,
             optimizer=self.optimizer,
             num_warmup_steps=cfg.training.lr_warmup_steps,
-            num_training_steps=(
-                len(train_dataloader) * cfg.training.num_epochs) \
-                    // cfg.training.gradient_accumulate_every,
+            num_training_steps=(len(train_dataloader) * cfg.training.num_epochs)
+            // cfg.training.gradient_accumulate_every,
             # pytorch assumes stepping LRScheduler every epoch
             # however huggingface diffusers steps it every batch
-            last_epoch=self.global_step-1
+            last_epoch=self.global_step - 1,
         )
 
         # configure ema
         ema: EMAModel = None
         if cfg.training.use_ema:
-            ema = hydra.utils.instantiate(
-                cfg.ema,
-                model=self.ema_model)
-
-        
+            ema = hydra.utils.instantiate(cfg.ema, model=self.ema_model)
 
         cfg.logging.name = str(cfg.logging.name)
         cprint("-----------------------------", "yellow")
@@ -134,7 +130,7 @@ class iDP3Workspace(BaseWorkspace):
         wandb_run = wandb.init(
             dir=str(self.output_dir),
             config=OmegaConf.to_container(cfg, resolve=True),
-            **cfg.logging
+            **cfg.logging,
         )
         wandb.config.update(
             {
@@ -144,8 +140,7 @@ class iDP3Workspace(BaseWorkspace):
 
         # configure checkpoint
         topk_manager = TopKCheckpointManager(
-            save_dir=os.path.join(self.output_dir, 'checkpoints'),
-            **cfg.checkpoint.topk
+            save_dir=os.path.join(self.output_dir, "checkpoints"), **cfg.checkpoint.topk
         )
 
         # device transfer
@@ -158,27 +153,35 @@ class iDP3Workspace(BaseWorkspace):
         # save batch for sampling
         train_sampling_batch = None
 
-
         # training loop
-        log_path = os.path.join(self.output_dir, 'logs.json.txt')
+        log_path = os.path.join(self.output_dir, "logs.json.txt")
         with JsonLogger(log_path) as json_logger:
-            for local_epoch_idx in tqdm.tqdm(range(cfg.training.num_epochs), desc=f"Training"):
+            for local_epoch_idx in tqdm.tqdm(
+                range(cfg.training.num_epochs), desc=f"Training"
+            ):
                 step_log = dict()
                 # ========= train for this epoch ==========
                 train_losses = list()
                 for batch_idx, batch in enumerate(train_dataloader):
                     t1 = time.time()
                     # device transfer
-                    batch = dict_apply(batch, lambda x: x.to(device, non_blocking=True) if isinstance(x, torch.Tensor) else x)
+                    batch = dict_apply(
+                        batch,
+                        lambda x: (
+                            x.to(device, non_blocking=True)
+                            if isinstance(x, torch.Tensor)
+                            else x
+                        ),
+                    )
                     if train_sampling_batch is None:
                         train_sampling_batch = batch
-                
+
                     # compute loss
                     t1_1 = time.time()
                     raw_loss, loss_dict = self.model.compute_loss(batch)
                     loss = raw_loss / cfg.training.gradient_accumulate_every
                     loss.backward()
-                    
+
                     t1_2 = time.time()
 
                     # step optimizer
@@ -195,15 +198,15 @@ class iDP3Workspace(BaseWorkspace):
                     raw_loss_cpu = raw_loss.item()
                     train_losses.append(raw_loss_cpu)
                     step_log = {
-                        'train_loss': raw_loss_cpu,
-                        'global_step': self.global_step,
-                        'epoch': self.epoch,
-                        'lr': lr_scheduler.get_last_lr()[0]
+                        "train_loss": raw_loss_cpu,
+                        "global_step": self.global_step,
+                        "epoch": self.epoch,
+                        "lr": lr_scheduler.get_last_lr()[0],
                     }
                     t1_5 = time.time()
                     step_log.update(loss_dict)
                     t2 = time.time()
-                    
+
                     if verbose:
                         print(f"total one step time: {t2-t1:.3f}")
                         print(f" compute loss time: {t1_2-t1_1:.3f}")
@@ -211,56 +214,67 @@ class iDP3Workspace(BaseWorkspace):
                         print(f" update ema time: {t1_4-t1_3:.3f}")
                         print(f" logging time: {t1_5-t1_4:.3f}")
 
-                    is_last_batch = (batch_idx == (len(train_dataloader)-1))
+                    is_last_batch = batch_idx == (len(train_dataloader) - 1)
                     if not is_last_batch:
                         # log of last step is combined with validation and rollout
                         wandb_run.log(step_log, step=self.global_step)
                         json_logger.log(step_log)
                         self.global_step += 1
 
-                    if (cfg.training.max_train_steps is not None) \
-                        and batch_idx >= (cfg.training.max_train_steps-1):
+                    if (cfg.training.max_train_steps is not None) and batch_idx >= (
+                        cfg.training.max_train_steps - 1
+                    ):
                         break
 
                 # at the end of each epoch
                 # replace train_loss with epoch average
                 train_loss = np.mean(train_losses)
-                step_log['train_loss'] = train_loss
+                step_log["train_loss"] = train_loss
 
                 # ========= eval for this epoch ==========
                 policy = self.model
                 if cfg.training.use_ema:
                     policy = self.ema_model
                 policy.eval()
-                
-                    
+
                 # run validation
                 if (self.epoch % cfg.training.val_every) == 0:
-                    with torch.no_grad():                        
+                    with torch.no_grad():
                         train_losses = list()
-                        
+
                         for batch_idx, batch in enumerate(train_dataloader):
-                            batch = dict_apply(batch, lambda x: x.to(device, non_blocking=True) if isinstance(x, torch.Tensor) else x)
-                            obs_dict = batch['obs']
-                            gt_action = batch['action']
+                            batch = dict_apply(
+                                batch,
+                                lambda x: (
+                                    x.to(device, non_blocking=True)
+                                    if isinstance(x, torch.Tensor)
+                                    else x
+                                ),
+                            )
+                            obs_dict = batch["obs"]
+                            gt_action = batch["action"]
 
                             result = policy.predict_action(obs_dict)
-                            pred_action = result['action_pred']
+                            pred_action = result["action_pred"]
                             mse = torch.nn.functional.mse_loss(pred_action, gt_action)
                             train_losses.append(mse.item())
-                            
-                            if (cfg.training.max_train_steps is not None) \
-                                and batch_idx >= (cfg.training.max_train_steps-1):
+
+                            if (
+                                cfg.training.max_train_steps is not None
+                            ) and batch_idx >= (cfg.training.max_train_steps - 1):
                                 break
                         train_loss = np.sum(train_losses)
                         # log epoch average validation loss
-                        step_log['train_action_mse_error'] = train_loss
-                        step_log['test_mean_score'] = - step_log['train_action_mse_error']
+                        step_log["train_action_mse_error"] = train_loss
+                        step_log["test_mean_score"] = -step_log[
+                            "train_action_mse_error"
+                        ]
                         cprint(f"val loss: {train_loss:.7f}", "cyan")
 
-
                 # checkpoint
-                if (self.epoch % cfg.training.checkpoint_every) == 0 and cfg.checkpoint.save_ckpt:
+                if (
+                    self.epoch % cfg.training.checkpoint_every
+                ) == 0 and cfg.checkpoint.save_ckpt:
                     # checkpointing
                     if cfg.checkpoint.save_last_ckpt:
                         self.save_checkpoint()
@@ -270,9 +284,9 @@ class iDP3Workspace(BaseWorkspace):
                     # sanitize metric names
                     metric_dict = dict()
                     for key, value in step_log.items():
-                        new_key = key.replace('/', '_')
+                        new_key = key.replace("/", "_")
                         metric_dict[new_key] = value
-                    
+
                     # We can't copy the last checkpoint here
                     # since save_checkpoint uses threads.
                     # therefore at this point the file might have been empty!
@@ -288,43 +302,43 @@ class iDP3Workspace(BaseWorkspace):
                 # log of last step is combined with validation and rollout
                 wandb_run.log(step_log, step=self.global_step)
                 json_logger.log(step_log)
-                
+
                 self.global_step += 1
                 self.epoch += 1
                 del step_log
 
         # stop wandb run
         wandb_run.finish()
-    
+
     def get_model(self):
         cfg = copy.deepcopy(self.cfg)
-        
+
         tag = "latest"
         # tag = "best"
         lastest_ckpt_path = self.get_checkpoint_path(tag=tag)
-        
+
         if lastest_ckpt_path.is_file():
-            cprint(f"Resuming from checkpoint {lastest_ckpt_path}", 'magenta')
+            cprint(f"Resuming from checkpoint {lastest_ckpt_path}", "magenta")
             self.load_checkpoint(path=lastest_ckpt_path)
         lastest_ckpt_path = str(lastest_ckpt_path)
 
         policy = self.model
         if cfg.training.use_ema:
-            policy = self.ema_model    
+            policy = self.ema_model
         policy.eval()
 
         return policy
 
-        
 
 @hydra.main(
-    config_path=str(pathlib.Path(__file__).parent.parent.joinpath("config")), 
-    config_name=pathlib.Path(__file__).stem)
-
+    config_path=str(pathlib.Path(__file__).parent.parent.joinpath("config")),
+    config_name=pathlib.Path(__file__).stem,
+)
 def main(cfg):
 
     workspace = iDP3Workspace(cfg)
     workspace.run()
+
 
 if __name__ == "__main__":
     main()
--- Improved-3D-Diffusion-Policy/train.py
+++ Improved-3D-Diffusion-Policy/train.py
@@ -3,6 +3,7 @@ Usage:
 Training:
 python train.py --config-name=train_diffusion_lowdim_workspace
 """
+
 import os
 from diffusion_policy_3d.workspace.base_workspace import BaseWorkspace
 import pathlib
@@ -11,11 +12,12 @@ import hydra
 from termcolor import cprint
 
 import sys
-# use line-buffering for both stdout and stderr
-sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
-sys.stderr = open(sys.stderr.fileno(), mode='w', buffering=1)
 
-os.environ['WANDB_SILENT'] = "True"
+# use line-buffering for both stdout and stderr
+sys.stdout = open(sys.stdout.fileno(), mode="w", buffering=1)
+sys.stderr = open(sys.stderr.fileno(), mode="w", buffering=1)
+
+os.environ["WANDB_SILENT"] = "True"
 
 # allow for detecting segmentation fault
 # import faulthandler
@@ -25,9 +27,11 @@ os.environ['WANDB_SILENT'] = "True"
 # allows arbitrary python code execution in configs using the ${eval:''} resolver
 OmegaConf.register_new_resolver("eval", eval, replace=True)
 
+
 @hydra.main(
-    config_path=str(pathlib.Path(__file__).parent.joinpath(
-        'diffusion_policy_3d', 'config'))
+    config_path=str(
+        pathlib.Path(__file__).parent.joinpath("diffusion_policy_3d", "config")
+    )
 )
 def main(cfg: OmegaConf):
     # resolve immediately so all the ${now:} resolvers
